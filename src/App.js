61a7eb986aa63212a59e823e1f0632eedc950535
Vasko <vbblajev@gmail.com>
2024-02-06 18:01:31 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+}
+
+function Logo() {
+  return <h1>🌴 Far Away 🎒</h1>;
+}
+
+function Form() {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your trip?</h3>
+    </div>
+  );
+}
+
+function PackingList() {
+  return (
+    <ul className="list">
+      {initialItems.map((item) => (
+        <Item item={item} key={item.id} />
+      ))}
+    </ul>
+  );
+}
+
+function Item({ item }) {
+  return <li>{item.description}</li>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em>You have X items on your list, and you already packed X (X%)</em>
+    </footer>
+  );
+}
